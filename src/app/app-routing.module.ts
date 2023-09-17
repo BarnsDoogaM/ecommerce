fb7b7891fd2620412bd9e17cc4ed694fb805747b
BarnsDoogaM <BarnsDooga@gmail.com>
2023-09-17 16:04:48 -0700

    header redesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products/products.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header', component: HeaderComponent},
+  {path: 'cart', component: CartComponent},
+  {
+    path: '', component: ProductsComponent
+  },
+  {
+    path: 'products', component: ProductsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products/products.component';
+import { CartComponent } from './cart/cart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    ProductsComponent,
+    CartComponent,
+  
   ],
   imports: [
     BrowserModule,
